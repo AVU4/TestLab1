0e48179d207e197929b8724b0b14b84acb9c9002
AVU4 <vai00@184.education>
2021-03-03 02:48:55 +0300

    Added some tests

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        MathFunction mathFunction = new MathFunction();
-        System.out.println(mathFunction.arcsinInRow(1.1));
-        System.out.println(Math.asin(1.1));
-    }
-}
